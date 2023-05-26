68bf74b1f6c698d40d4524313e449d20fccf2220
NK <oscarfu0721@gmail.com>
2023-05-26 02:38:53 +0100

    css

--- background.js
+++ background.js
@@ -1,15 +1,26 @@
-function updateTime() {
-  var date = new Date();
-  var h = date.getHours();
-  var m = date.getMinutes();
-  var s = date.getSeconds();
-  h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + m : m;
-  s = s < 10 ? "0" + s : s;
-  document.getElementsByClassName("clock")[0].innerHTML = `${h}:${m}:${s}`;
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+function t() {
+  var d = new Date();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString();
+  document.getElementById("date").innerHTML = `${d.toLocaleDateString("en-US", {
+    weekday: "long",
+  })} ${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
 }
-updateTime();
-setInterval(updateTime, 1000);
+t();
+window.setInterval(t, 1000);
 const songs = [
   {
     url: "https://cdn.discordapp.com/attachments/842014909264953354/1111362763739123863/h.mp4",
@@ -39,14 +50,14 @@ video.addEventListener("ended", chooseSong);
 chrome.storage.sync.get("options", function (data) {
   if (data.options.static) {
     video.style.display = "none";
-    document.getElementById("bg").style.display = "block";
-    document.getElementsByClassName("clock")[0].style["align-items"] =
-      "flex-start";
-    document.getElementsByClassName("clock")[0].style.top = "100px";
+    // document.getElementById("bg").style.display = "block";
+    // document.getElementsByClassName("clock")[0].style["align-items"] =
+    //   "flex-start";
+    // document.getElementsByClassName("clock")[0].style.top = "100px";
   } else {
     video.style.display = "block";
-    document.getElementById("bg").style.display = "none";
-    document.getElementsByClassName("clock")[0].style["align-items"] = "center";
+    // document.getElementById("bg").style.display = "none";
+    // document.getElementsByClassName("clock")[0].style["align-items"] = "center";
   }
 });
 
@@ -55,15 +66,15 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     chrome.storage.sync.set({ options: message.options }, function () {
       if (message.options.static) {
         video.style.display = "none";
-        document.getElementById("bg").style.display = "block";
-        document.getElementsByClassName("clock")[0].style["align-items"] =
-          "flex-start";
+        // document.getElementById("bg").style.display = "block";
+        // document.getElementsByClassName("clock")[0].style["align-items"] =
+        //   "flex-start";
       } else {
         video.style.display = "block";
         video.muted = true;
-        document.getElementById("bg").style.display = "none";
-        document.getElementsByClassName("clock")[0].style["align-items"] =
-          "center";
+        // document.getElementById("bg").style.display = "none";
+        // document.getElementsByClassName("clock")[0].style["align-items"] =
+        //   "center";
       }
       sendResponse({ success: true });
     });
